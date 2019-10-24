1c438a41b569bdb1f911c81de372cbac93e1cee8
Kaneda <kanedaoscube@gmail.com>
2019-10-24 10:02:17 +0700

    .

---
+++ js/search-hostel.js
@@ -0,0 +1,17 @@
+let searchOpen = document.querySelector(".search-hostel-open-btn");
+let searchForm = document.querySelector(".search-hostel");
+
+if (searchOpen) {
+  if (searchForm) {
+    searchForm.classList.add("search-hostel-hidden");
+  }
+  ;
+
+  searchOpen.addEventListener("click", function (event) {
+    event.preventDefault();
+    searchForm.classList.toggle("search-hostel-hidden");
+    searchForm.classList.toggle("search-hostel-show");
+  });
+}
+;
+
